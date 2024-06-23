9780488c7ead1c74ab9a67dae7eb5393fde6ec42
Clearance <clearancemorumudi@outlook.com>
2024-06-23 22:08:07 +0200

    adding the dark and light mode

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({
+
+const themeBtn = document.querySelector('.theme-btn')
+themeBtn.addEventListener('click', () => {
+    let element = document.body;
+    element.classList.toggle('light-mode')
+})
+
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
         behavior: 'smooth'
     });
 }
+
+const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
